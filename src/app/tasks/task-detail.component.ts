68f15caab65a241b82983b8b3605c3bde19ce058
Hammad Tariq <hammadtariq65@gmail.com>
2016-12-22 13:09:37 +0500

    initial structure

---
+++ src/app/tasks/task-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    template: `
+    <div>
+        <span>Some task detail to show up here.</span>
+    </div>
+    `
+})
+
+export class TaskDetailComponent {
+}
